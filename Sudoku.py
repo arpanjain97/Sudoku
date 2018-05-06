49a1776cd2439c13c2cd5f66ea7026dc86d555a8
arpanjain97 <arpanjain97@gmail.com>
2018-05-06 20:42:08 +0530

    Add layout adjustment to make UI adaptive

--- Sudoku.py
+++ Sudoku.py
@@ -99,13 +99,13 @@ class Timer_app():
     '''class for timer widget implementation'''
     def __init__(self,window):
         self.win=window
-        self.box=Label(self.win,text="00:00",font=(basefont,60,basestyle),bg=theme_color2,fg='white',relief="flat")
+        self.box=Label(self.win,text='00:00',font=(basefont,60,basestyle),bg=theme_color2,fg='white',relief='flat')
         self.box.place(x=950,y=265)
         self.count=-1
     def start(self):
         #start timer
         self.count+=1
-        disp=str(self.count/600)+str((self.count%600)/60)+":"+str((self.count%600%60)/10)+str(self.count%600%60%10)
+        disp=str(self.count/600)+str((self.count%600)/60)+':'+str((self.count%600%60)/10)+str(self.count%600%60%10)
         self.box.config(text=disp)
         self.chk=self.win.after(1000,self.start)
     def stop(self):
@@ -124,26 +124,36 @@ class Timer_app():
         #sets time
         self.count=val
         self.start()
+    def place(self, posX, posY):
+	self.box.place(x=posX, y=posY)
 class Grid():
     '''class dealing with sudoku grid as shown on screen'''
-    def __init__(self,root,y_pos=50,x_pos=100):
+    def __init__(self,root):
         #creates blank grid
+	self.root = root
         self.grid_items=[[0 for row in range(9)] for col in range(9)]
-        for row in range(9):
-            x_pos=100
+	for row in range(9):
             for col in range(9):
-                self.grid_items[row][col]=Entry(root,font=('Comic Sans Ms',30,'bold'),fg="red",width=2,disabledforeground="black",borderwidth=5,justify='center',relief='flat')
+                self.grid_items[row][col]=Entry(self.root,font=('Comic Sans Ms',30,'bold'),fg='red',width=2,disabledforeground='black',borderwidth=5,justify='center',relief='flat')
                 self.grid_items[row][col].bind('<FocusOut>',self.__valid)
+	self.place(100, 70)
+  
+    def place(self, originX, originY):
+        y_pos = originY
+	for row in range(9):
+            x_pos=originX
+            for col in range(9):
                 if col==3 or col==6:
                     x_pos+=10
-                self.grid_items[row][col].place(x=65*col+x_pos,y=70*row+y_pos)
+                self.grid_items[row][col].place(x=65*col+x_pos,y=60*row+y_pos)
             if row==2 or row==5:
                 y_pos+=10
+
     def __valid(self,event):
         #checks validity of entered values
             d=event.widget.get()
             if len(d)>1 or d not in ['','1','2','3','4','5','6','7','8','9']:
-                tkMessageBox.showwarning("Invalid Input","Enter a value between 1 and 9")
+                tkMessageBox.showwarning('Invalid Input','Enter a value between 1 and 9')
                 event.widget.delete(0,END)
     def show(self,seq,disp='que'):
         #shows the sudoku string as grid on screen
@@ -195,7 +205,7 @@ class Load():
             fil=open(os.path.join(data_folder, loc), 'rb')
             return fil
         except IOError as e:
-            tkMessageBox.showwarning("Open Directory","Unable to open Directory location: "+loc+"\nEither it doesnt exist or is corrupt."+str(e))
+            tkMessageBox.showwarning('Open Directory','Unable to open Directory location: '+loc+'\nEither it doesnt exist or is corrupt.'+str(e))
             raise
 class Save():
     #create a saving object and save it in data directory
@@ -250,53 +260,64 @@ class Controller(Sudoku,Grid):
     def __init__(self,root):
         #creates main window of programme and its menu
         self.root=root
-        self.base_icon = Image("photo", file = os.path.join(images_folder, base_icon_img))
+	self.full = False
+        self.base_icon = Image('photo', file = os.path.join(images_folder, base_icon_img))
         self.load_conn=Load()
         self.save_conn=Save()
         self.lvl_map={1:8,2:4,3:2}
         root.after(2,self.add_widgets)
-        root.protocol("WM_DELETE_WINDOW", self.s_exit)
+        root.protocol('WM_DELETE_WINDOW', self.s_exit)
         self.Window()
     def Window(self):
         #formats window and loads images
-        root.wm_title("SUDOKU")
+        root.wm_title('SUDOKU')
         root.minsize(width=1366,height=768)
-        #root.maxsize(width=1366,height=768)
         self.root.call('wm', 'iconphoto', self.root._w, self.base_icon)
         self.back=PhotoImage(file = os.path.join(images_folder, base_img))
         self.canv=Canvas(self.root,height=766,width=1366)
-        self.canv.place(x=0,y=0)
+        self.canv.pack(fill = BOTH, expand = 1)
         self.canv.create_image(0,0,image=self.back,anchor='nw')
         self.menubar=Menu(self.root)
         self.root.config(menu=self.menubar)
         self.title=PhotoImage(file = os.path.join(images_folder, 'title.gif'))
-        self.canv.create_image(750,65,image=self.title,anchor='nw')
+        self.canv.create_image(750,65,image=self.title,anchor='nw',tag='titleBanner')
     def add_widgets(self):
         #adds the required widgets to window
-        self.grid=Grid(self.root)
-        self.timer=Timer_app(self.root)
-        Button(self.root,text='SUBMIT',relief='flat',font=(basefont,30,basestyle),fg='white',bg=theme_color2,command=self.check).place(x=975,y=450)
+        self.grid = Grid(self.canv)
+        self.timer=Timer_app(self.canv)
+        self.submit = Button(self.canv,text='SUBMIT',relief='flat',font=(basefont,30,basestyle),fg='white',bg=theme_color2,command=self.check)
+	self.submit.place(x=975,y=450)
         filemenu=Menu(self.menubar,tearoff=0)
-        filemenu.add_command(label="New",command=self.new)
-        filemenu.add_command(label="Restart",command=self.restart)
-        filemenu.add_command(label="Load",command=self.loader)
-        filemenu.add_command(label="Save",command=self.save)
-        filemenu.add_command(label="Solution",command=self.solv)
+        filemenu.add_command(label='New',command=self.new)
+        filemenu.add_command(label='Restart',command=self.restart)
+        filemenu.add_command(label='Load',command=self.loader)
+        filemenu.add_command(label='Save',command=self.save)
+        filemenu.add_command(label='Solution',command=self.solv)
         filemenu.add_separator()
-        filemenu.add_command(label="Exit",command=self.s_exit)
-        self.menubar.add_cascade(label="MENU",menu=filemenu)
-        self.menubar.add_command(label="Fullscreen",command=self.fullscreen)
-        self.menubar.add_command(label=" View Help",command=self.help_d)
+        filemenu.add_command(label='Exit',command=self.s_exit)
+        self.menubar.add_cascade(label='MENU',menu=filemenu)
+        self.menubar.add_command(label='Toggle Fullscreen',command=self.fullscreen)
+        self.menubar.add_command(label=' View Help',command=self.help_d)
         self.root.bind('<F5>',self.help_d)
         self.root.bind('<Escape>',self.s_exit)
+	self.canv.bind('<Configure>', self.resize)
+    def resize(self, event):
+        w , h = event.width , event.height
+        if(w > 1200):              #condition to filter required event
+                x, y = (w-1366)/2, (h-768)*1/2
+		self.grid.place(x+100,y+70)
+		self.timer.place(x+950,y+265)
+		self.submit.place(x=x+975,y=y+450)
+		self.canv.coords("titleBanner",x+750,y+65)
     def s_exit(self,event=None):
         #safe exit func
-        if not tkMessageBox.askyesno("Exit","Do you want to quit without saving ?"):
+        if not tkMessageBox.askyesno('Exit','Do you want to quit without saving ?'):
             return None
         self.root.destroy()
     def fullscreen(self):
-        #fullscreen func
-        self.root.wm_attributes("-fullscreen",True)
+        # fullscreen function
+	self.full = not self.full
+        self.root.wm_attributes('-fullscreen',self.full)
     def help_d(self,event=None):
         #shows help document
         try:
@@ -339,9 +360,9 @@ class Controller(Sudoku,Grid):
         self.lvl = IntVar()
         self.master=Toplevel(bg=theme_color)
         self.root.call('wm', 'iconphoto', self.master._w, self.base_icon)
-        Radiobutton(self.master, text="Easy   ",fg='green',bg=theme_color,offrelief='flat',variable=self.lvl,indicatoron=0,font=fontu,value=1,command=self.create).pack(anchor=W)
-        Radiobutton(self.master, text="Medium",fg='yellow',bg=theme_color,offrelief='flat',variable=self.lvl,indicatoron=0,font=fontu,value=2,command=self.create).pack(anchor=W)
-        Radiobutton(self.master, text="Hard   ",fg='red3',bg=theme_color,offrelief='flat',variable=self.lvl,indicatoron=0,font=fontu,value=3,command=self.create).pack(anchor=W)
+        Radiobutton(self.master, text='Easy   ',fg='green',bg=theme_color,offrelief='flat',variable=self.lvl,indicatoron=0,font=fontu,value=1,command=self.create).pack(anchor=W)
+        Radiobutton(self.master, text='Medium',fg='yellow',bg=theme_color,offrelief='flat',variable=self.lvl,indicatoron=0,font=fontu,value=2,command=self.create).pack(anchor=W)
+        Radiobutton(self.master, text='Hard   ',fg='red3',bg=theme_color,offrelief='flat',variable=self.lvl,indicatoron=0,font=fontu,value=3,command=self.create).pack(anchor=W)
     def create(self):
         #adjusts sudoku as per level and display
         hint_no=self.lvl_map[self.lvl.get()]
@@ -375,7 +396,7 @@ class Controller(Sudoku,Grid):
            self.saved=fil.readlines()
            for i in self.saved:
                self.load_opt.insert(END,i[:-1])
-           Button(self.load_win, bg = theme_color, relief = 'flat', text = "LOAD", font = (basefont, 30, basestyle), command = self.load, fg = 'purple4').pack()
+           Button(self.load_win, bg = theme_color, relief = 'flat', text = 'LOAD', font = (basefont, 30, basestyle), command = self.load, fg = 'purple4').pack()
            fil.close()
        except:
             pass
